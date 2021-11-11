032c7eadcf1e879cd035b3fe486d5ae1e435f658
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-11 12:29:27 -0300

    Update questao01.c

--- questao01.c
+++ questao01.c
@@ -1,1 +1,11 @@
+Aluno: Vinícius Venceslau Venancio da Penha
 
+Docente: Agostinho Brito
+
+Engenharia Elétrica - UFRN
+
+Lista 1 – Programação Avançada
+
+Questão 1)
+
+Resposta: Tendo em vista que a programação faz parte do desenvolvimento de projetos e serviços prestados pelas engenharias, é possível afirmar que o conteúdo de ponteiros é essencial, pois facilita o procedimento de criação e composição dos códigos. Nesse contexto, o ponteiro torna possível identificar e modificar um determinado endereço de memória.
