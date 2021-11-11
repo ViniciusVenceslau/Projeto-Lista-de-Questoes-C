18a8de4b0f7bb910bd4bfc446e1d9cb40a007e63
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-11 13:25:38 -0300

    Update questao11.c

--- questao11.c
+++ questao11.c
@@ -1,17 +1,17 @@
 Questão 11)
 
-a)	aloha[2] = value;
+a)	aloha[2] = value; válido.
 
-b)	scanf("%f", &aloha); 
+b)	scanf("%f", &aloha); válido.
 
-c)	aloha = "value"; inválido
+c)	aloha = "value"; inválido.
 
-d)	printf("%f", aloha); 
+d)	printf("%f", aloha); válido.
 
-e)	coisas[4][4] = aloha[3]; 
+e)	coisas[4][4] = aloha[3]; válido.
 
-f)	coisas[5] = aloha; inválido
+f)	coisas[5] = aloha; inválido.
 
-g)	pf = value; inválido
+g)	pf = value; inválido.
 
-h)	pf = aloha;
+h)	pf = aloha; válido.
