87e06e36cd5c0ba32387a322e6249246a8a80ab3
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-11 12:43:23 -0300

    Update questao05.c

--- questao05.c
+++ questao05.c
@@ -1,1 +1,28 @@
+Questão 5)
+
+a)	20 – Esperada.
+
+b)	29 – Esperada.
+
+c)	P – Esperada.
+
+d)	e – Esperada.
+
+e)	P – Esperada (retornou a primeira letra, pois ao escrever p3 = nome, e imprimir na tela “*p3”, se tem como resultado a primeira letra da string.)
+
+f)	e – Esperada.
+
+g)	t – Esperada (Tendo em vista que, anteriormente, p3 estava associada a letra “e”, ao decrementar uma unidade na variável p3, retorna na tela a letra anterior.)
+
+h)	31 – Esperada (retornou o número da posição inicial do vetor. )
+
+i)	45 – Esperada.
+
+j)	27 – Esperada.
+
+k)	31 – Esperada (uma vez que a variável p4 está relacionada ao último elemento do vetor e, posteriormente, ocorre um discremento de 2, obtém-se o valor de vetor[0].) 
+
+l)	45 – Esperada.
+
+m)	45 – Esperada (repetiu o valor da variável anterior.)
 
