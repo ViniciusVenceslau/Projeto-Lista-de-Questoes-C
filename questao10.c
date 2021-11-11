f161f9b678c2b894f9654419351b92c0e3a4aba2
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-11 12:57:31 -0300

    Update questao10.c

--- questao10.c
+++ questao10.c
@@ -1,1 +1,13 @@
+Questão 10)
 
+a)	x for declarado como char? Resposta: 4093, 4094 e 4095.
+
+b)	x for declarado como int? Resposta: 4094, 4096 e 4098.
+
+c)	x for declarado como float? Resposta: 4096, 4100 e 4104.
+
+d)	x for declarado como double? Resposta: 4100, 4108 e 4116.
+
+Imagem ilustrativa do código desenvolvido por mim:
+
+![image](https://user-images.githubusercontent.com/92820372/140997475-c9ca8175-9f0e-4656-904f-31c2ae4268df.png)
