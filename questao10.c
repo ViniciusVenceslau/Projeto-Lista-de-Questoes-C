5681da24fbe8188ac284f24f4dd04637185dff60
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-11 13:02:02 -0300

    Update questao10.c

--- questao10.c
+++ questao10.c
@@ -7,5 +7,3 @@ b)	x for declarado como int? Resposta: 4094, 4096 e 4098.
 c)	x for declarado como float? Resposta: 4096, 4100 e 4104.
 
 d)	x for declarado como double? Resposta: 4100, 4108 e 4116.
-
-Imagem ilustrativa do código desenvolvido por mim:
