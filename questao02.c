deefd42650c430d5d70873591cfbdf2df86d634d
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-11 12:31:57 -0300

    Update questao02.c

--- questao02.c
+++ questao02.c
@@ -1,1 +1,9 @@
+Questão 2) 
 
+a)	p == &i; Resposta: Verdadeiro, “1”.
+
+b)	*p - *q; Resposta: 3 – 5, ou seja, -2.
+
+c)	**&p; Resposta: Esta expressão tem valor igual a 3.
+
+d)	3 - *p/(*q) + 7; Resposta: 3 + 7, ou seja, 10. Isso ocorre, pois *p/(*q) deveria ser 0.6, mas divisão de valores inteiros resulta um inteiro, neste caso, 0.
