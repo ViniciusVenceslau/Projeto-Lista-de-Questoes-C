07a778a1f6ab089ca2c5bc05cd240f628cba9762
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-11 12:46:56 -0300

    Update questao08.c

--- questao08.c
+++ questao08.c
@@ -1,1 +1,9 @@
+Questão 8)
 
+a) Válida, pois “p” está associado ao endereço de memória da segunda posição do vetor, ou seja, &mat[1].
+
+b) Válida, pois “p” relaciona-se com o endereço de memória do primeiro elemento do vetor, ou seja, &mat[0].
+
+c) Válida, pois corresponde a mesma expressão do item anterior.
+
+d) Válida, a variável “x” passa a ter o valor correspondente ao primeiro elemento do vetor mat, ou seja, mat[0].
