ca5932b613cbf1d286c1fb209d704bf08ab03080
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-11 13:03:40 -0300

    Update questao12.c

--- questao12.c
+++ questao12.c
@@ -1,1 +1,3 @@
+Questão 12)
 
+Resposta: O ponteiro em uma função é usado, comumente, como argumento da função. Nesse viés, através do ponteiro é possível reduzir o espaço de memória necessário para compilação/ execução do código. Isto ocorre, pois a variável ponteiro apontará para um endereço de memória, ao invés de ser necessário “duplicar” o conteúdo presente em uma variável qualquer.
