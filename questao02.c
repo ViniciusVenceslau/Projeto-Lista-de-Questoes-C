ba2c2d068ff278cddec8ff7fd26b521ff59a1759
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-11 12:41:18 -0300

    Update questao02.c

--- questao02.c
+++ questao02.c
@@ -6,4 +6,4 @@ b)	*p - *q; Resposta: 3 – 5, ou seja, -2.
 
 c)	**&p; Resposta: Esta expressão tem valor igual a 3.
 
-d)	3 - *p/(*q) + 7; Resposta: 3 + 7, ou seja, 10. Isso ocorre, pois *p/(*q) deveria ser 0.6, mas divisão de valores inteiros resulta um inteiro, neste caso, 0.
+d)	3 - *p/(*q) + 7; Resposta: 3 + 7, ou seja, 10. Isso ocorre, pois "*p/(*q)" deveria ser 0.6, entretanto a divisão de variáveis do tipo inteiro resulta num valor inteiro, neste caso, 0.
