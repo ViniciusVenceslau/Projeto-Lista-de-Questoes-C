2017ebb0dae1cd6e5d34b538ed3da52654e1c670
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-16 14:49:55 -0300

    Update questao12.c

--- questao12.c
+++ questao12.c
@@ -1,3 +1,3 @@
 Questão 12)
 
-Resposta: O ponteiro para função em C serve, comumente, para definir que função será executada naquela instante, não sendo necessário informar o nome da função de forma explícita, dado que o ponteiro terá a capacidade de apontar para a função desejada no decorrer do código.
+Resposta: O ponteiro para função em C serve, comumente, para definir que função será executada naquele instante, não sendo necessário informar o nome da função de forma explícita, dado que o ponteiro terá a capacidade de apontar para a função desejada no decorrer do código.
