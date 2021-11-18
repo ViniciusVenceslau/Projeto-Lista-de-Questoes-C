9f96e50aa30a370828f8b988ea703d062b15c74a
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-18 16:08:33 -0300

    Código questão 14

--- questao14.c
+++ questao14.c
@@ -11,3 +11,39 @@ Linha 27 é utilizado a função "qsort()" para ordenar de forma crescente os el
 Linhas 28 a 31 é exibido na tela do usuário os valores de maneira crescente.
 Linha 33 ocorre a liberação do espaço de memória utilizado para compor o vetor por meio da função "free()".
 Código da questão 14 a seguir:
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare (const void *a, const void *b){
+return (*(float*)a - *(float*)b);
+}
+
+int comparefloat (const void *a, const void *b){
+if(*(float*)a > *(float*)b){
+return 1;
+} else if (*(float*)a == *(float*)b){
+return 0;
+} else {
+return -1;
+}
+}
+
+int main() {
+int q;
+float *x;
+printf("Informe o tamanho do vetor:\n");
+scanf("%d", &q);
+x = malloc(q * sizeof(float));
+printf("Insira os elementos do vetor:\n");
+for(int i = 0; i < q; i++){
+scanf("%f", &x[i]);
+}
+qsort(x, q, sizeof(float), compare);
+printf("Os valores organizados em ordem crescente é:\n");
+for(int i = 0; i < q; i++){
+printf("%f", x[i]);
+printf("\n");
+}
+free(x);
+return 0;
+}
