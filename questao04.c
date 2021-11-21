bba0f249355ce2d1a0dabb4e7dceb0e586ba9b6c
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-21 16:19:01 -0300

    Update questao04.c

--- questao04.c
+++ questao04.c
@@ -1,18 +1,17 @@
 Questão 4)
 
-a)	p = &i;
+a)	p = &i; legal
 
-b)	*q = &j;
+b)	*q = &j; legal
 
-c)	p = &*&i; 
+c)	p = &*&i; legal 
 
 d)	i = (*&)j;  ilegal
 
-e)	i = *&j; 
+e)	i = *&j; legal
 
 f)	i = &&j; ilegal
 
 g)	q = *p; ilegal
 
-h)	i = (*p) + *q;
-
+h)	i = (*p) + *q; legal
