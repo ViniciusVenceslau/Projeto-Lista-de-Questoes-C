fe5e8ccd195929126d146847b9e5dd379d41f6c6
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-21 16:17:06 -0300

    Update questao01.c

--- questao01.c
+++ questao01.c
@@ -8,4 +8,4 @@ Lista 1 – Programação Avançada
 
 Questão 1)
 
-Resposta: Tendo em vista que a programação faz parte do desenvolvimento de projetos e serviços prestados pelas engenharias, é possível afirmar que o conteúdo de ponteiros é essencial, pois facilita o procedimento de criação e composição dos códigos. Nesse contexto, o ponteiro torna possível identificar e modificar um determinado endereço de memória.
+Resposta: Tendo em vista que a programação faz parte do desenvolvimento de projetos e serviços prestados pelas engenharias, é possível afirmar que o conteúdo de ponteiros é essencial, pois facilita o procedimento de criação e composição dos códigos. Nesse contexto, o ponteiro torna possível identificar um determinado endereço de memória e modificar o seu conteúdo.
