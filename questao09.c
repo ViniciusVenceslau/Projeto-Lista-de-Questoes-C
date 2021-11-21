422da27e8e5644f6043aa920b1c91756d48c1d90
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-21 16:20:10 -0300

    Update questao09.c

--- questao09.c
+++ questao09.c
@@ -1,3 +1,3 @@
 Questão 9)
 
-Resposta: O primeiro programa exibe na tela do usuário os elementos presentes no vetor “vet”, neste caso, os valores apresentados são: 4, 9, 13. Em contrapartida, o programa número dois expõe na tela os endereços de memórias  de cada elemento do vetor, assim, por ser um vetor do tipo “inteiro”, a distância entre os endereços varia de 4 em 4 bytes.
+Resposta: O primeiro programa exibe na tela do usuário os elementos presentes no vetor “vet”, neste caso, os valores apresentados são: 4, 9, 13. Em contrapartida, o programa número dois expõe na tela os endereços de memórias de cada elemento do vetor, assim, por ser um vetor do tipo “inteiro”, a distância entre os endereços varia de 4 em 4 bytes.
