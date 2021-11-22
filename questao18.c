8a819e9afe3bd78d77feec935083ee6264633800
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-22 18:08:01 -0300

    Update questao18.c

--- questao18.c
+++ questao18.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
+#include <math.h>
 
+void produto (int l, int c, int q, int matriz1[l][c], int matriz2[c][q], int matriz3[l][q]){
+
+//Produto AB
+for(int i=0; i<3; i++){
+	for(int j=0; j<3; j++){
+    matriz3[i][j] = 0;
+		for(int k=0; k<3; k++){
+			matriz3[i][j] = matriz3[i][j] + matriz1[i][k]*matriz2[k][j];
+		}
+	}
+}
+printf("O produto entre as matrizes (AB) é:\n");
+for(int i=0; i<3; i++){
+	for(int j=0; j<3; j++){
+		printf("%d ", matriz3[i][j]);
+  }
+  printf("\n");
+}
+}
+
+int main() {
+int l, c, q;
+printf("Informe o número de linhas da matriz A:\n");
+scanf("%d", &l);
+printf("Insira o número de colunas da matriz A:\n");
+scanf("%d", &c);
+printf("A matriz B terá %d linhas para poder ser feito o produto AB.\n", c);
+printf("Digite o número de colunas da matriz B:\n");
+scanf("%d", &q);
+
+int matrizA[l][c];
+int matrizB[c][q];
+int matrizC[l][q];
+
+printf("Informe os elementos da primeira matriz:\n");
+for(int i=0; i<3; i++){
+	for(int j=0; j<3; j++){
+		scanf("%d", &matrizA[i][j]);
+	}
+}
+printf("Informe os elementos da segunda matriz:\n");
+for(int i=0; i<3; i++){
+	for(int j=0; j<3; j++){
+		scanf("%d", &matrizB[i][j]);
+	}
+}
+produto(l, c, q, matrizA, matrizB, matrizC);
+return 0;
+}
