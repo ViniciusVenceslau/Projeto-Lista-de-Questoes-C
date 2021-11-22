d0267704e7e7b7f82dc8d8996b22ac8780404110
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-22 12:56:26 -0300

    Update questao15.c

--- questao15.c
+++ questao15.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int compare (const void *a, const void *b){
-return (*(float*)a - *(float*)b);
-}
-
 int comparefloat (const void *a, const void *b){
 if(*(float*)a > *(float*)b){
 return 1;
@@ -14,11 +10,23 @@ return 0;
 return -1;
 }
 }
+void ordenacao(float *x, int q, int (*comparefloat)(const void *a, const void *b)){
+int aux;
+for(int i = 0; i < q; i++){
+  for(int j = 0; j < q; j++){
+    if(comparefloat(x+i, x+j) < 0){
+      aux = x[i];
+      x[i] = x[j];
+      x[j] = aux;
+    }
+  }
+}
+}
 
 int main() {
 int q;
 float *x;
-int (*pf) (const void *, const void *) = compare;
+int (*pf) (const void *, const void *) = comparefloat;
 printf("Informe o tamanho do vetor:\n");
 scanf("%d", &q);
 x = malloc(q * sizeof(float));
@@ -26,7 +34,7 @@ printf("Insira os elementos do vetor:\n");
 for(int i = 0; i < q; i++){
 scanf("%f", &x[i]);
 }
-qsort(x, q, sizeof(float), *pf);
+ordenacao(x, q, *pf);
 printf("Os valores organizados em ordem crescente é:\n");
 for(int i = 0; i < q; i++){
 printf("%f", x[i]);
