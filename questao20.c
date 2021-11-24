1af4ffaa0bad0d2afe447d9a3381c765fdb1cf69
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2021-11-23 23:38:16 -0300

    Update questao20.c

--- questao20.c
+++ questao20.c
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 #include <time.h>
 int compare(const void * a, const void * b){
-  if ( *(float*)a >  (float*)b ) return 1;
-  if ( *(float*)a == (float*)b ) return 0;
-  if ( (float*)a <  (float*)b ) return -1;
+  if ( *(float*)a > *(float*)b ) return 1;
+  if ( *(float*)a == *(float*)b ) return 0;
+  if ( *(float*)a < *(float*)b ) return -1;
 int main(){
   float iT1, fT1, iT2, fT2, *x, *y;
   printf("Informe o tamanho do vetor:\n");
   scanf("%d", &q);
   iT1 = clock();
   x = GC_MALLOC(q * sizeof(float));
-  initT2 = clock();
+  iT2 = clock();
   y = malloc(q * sizeof(float));
   printf("Insira os elementos do vetor:\n");
   for(int i = 0; i < q; i++){
@@ -21,18 +21,18 @@ int main(){
   }
   printf("Valores digitados: \n");
   for(int i = 0; i < q; i++){
-    printf("%.1f ", x[i]);
+    printf("%f ", x[i]);
   }
   qsort(x, q, sizeof(float), compare); 
   printf("Elementos ordenados em ordem crescente: \n");
   for(i = 0; i < q; i++){
-    printf("%.1f ", x[i]);
+    printf("%f ", x[i]);
   }
   free(y);
-  finalT1 = clock();
-  finalT2 = clock();
-  printf("\nTempo de execucao1 = %.10f segundos", (fT1 - iT1)/CLOCKS_PER_SEC);
-  printf("\nTempo de execucao2 = %.10f segundos", (fT2 - iT2)/CLOCKS_PER_SEC);
-  printf("\nAtraso do coletor de lixo = %.10f segundos\n", ((fT1 - iT1)/CLOCKS_PER_SEC)-((fT2 - iT2)/CLOCKS_PER_SEC));
+  fT1 = clock();
+  fT2 = clock();
+  printf("\nTempo de execucao1 = %f segundos", (fT1 - iT1)/CLOCKS_PER_SEC);
+  printf("\nTempo de execucao2 = %f segundos", (fT2 - iT2)/CLOCKS_PER_SEC);
+  printf("\nAtraso do coletor de lixo = %f segundos\n", ((fT1 - iT1)/CLOCKS_PER_SEC)-((fT2 - iT2)/CLOCKS_PER_SEC));
   return 0;
 }
